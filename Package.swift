654eb310e221a05061d43493f24c505802637274
InfiniteFalltrough <golubenkovvs@gmail.com>
2025-11-03 19:01:23 +0700

    Package.swift template

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version: 6.2
+import PackageDescription
+
+let package = Package(
+    name: "RoutingTableKit",
+    defaultLocalization: "en",
+    platforms: [
+        .iOS(.v15),
+        .macOS(.v11),
+        .tvOS(.v15)
+    ],
+    products: [
+        .library(
+            name: "RoutingTableKit",
+            targets: ["RoutingTableKit"]
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "RoutingTableKit",
+            url: "https://github.com/InfiniteFalltrough/RoutingTableKitPackage/releases/download/v1.0.0/RoutingTableKit.xcframework.zip",
+            checksum: ""
+        )
+    ]
+)
